dbf724f374660125d9c6f207c4211f83cf98b3d8
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-19 10:05:30 -0600

    feat: Completed and tested Bank library create file exercises

--- CreateFile/CreateFileForm.Designer.cs
+++ CreateFile/CreateFileForm.Designer.cs
@@ -28,20 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(64, 432);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(104, 32);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save As";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(248, 432);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(104, 32);
+            this.enterButton.TabIndex = 9;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(432, 432);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(104, 32);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // CreateFileForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 511);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.saveButton);
+            this.Margin = new System.Windows.Forms.Padding(11, 12, 11, 12);
             this.Name = "CreateFileForm";
             this.Text = "Create File Form";
+            this.Controls.SetChildIndex(this.saveButton, 0);
+            this.Controls.SetChildIndex(this.enterButton, 0);
+            this.Controls.SetChildIndex(this.exitButton, 0);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- CreateFile/CreateFileForm.cs
+++ CreateFile/CreateFileForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,127 @@ namespace CreateFile
 {
     public partial class CreateFileForm : BankUIForm
     {
+        private StreamWriter fileWriter;  // Writes data to text file
+
+        // Parameterless constructor
         public CreateFileForm()
         {
             InitializeComponent();
+        }
+
+        // Event handler for Save Button
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // Create and show dialog box enabling user to save file
+            DialogResult result;  // Result of SaveFileDialog
+            string fileName;  // Name of file containing data
+
+            using (var fileChooser = new SaveFileDialog())
+            {
+                fileChooser.CheckFileExists = false;  // Let user create file
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName; // Name of file to save data
+            }
+
+            // Ensure that user clicked "OK"
+            if (result == DialogResult.OK)
+            {
+                // Show error if user specified invalid file type
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    MessageBox.Show("Invalid File Name", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    // Save file via FileStream
+                    try
+                    {
+                        // Save file with write access
+                        var output = new FileStream(fileName,
+                            FileMode.OpenOrCreate, FileAccess.Write);
+
+                        // Set s file to where data is written
+                        fileWriter = new StreamWriter(output);
+
+                        // Disable Save Button and enable Enter Button
+                        saveButton.Enabled = false;
+                        enterButton.Enabled = true;
+                    }
+                    catch (IOException)
+                    {
+                        // Notify user if file does not exist
+                        MessageBox.Show("Error opening file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            // Store TextBox values string array
+            string[] values = GetTextBoxValues();
+
+            // Determine whether TextBox account field is empty
+            if (!string.IsNullOrEmpty(values[(int)TextBoxIndices.Account]))
+            {
+                // Store TextBox values in Record and output it
+                try
+                {
+                    // Get account-number value from TextBox
+                    int accountNumber = int.Parse(values[(int)TextBoxIndices.Account]);
+
+                    // Determine whether accountNumber is valid
+                    if (accountNumber > 0)
+                    {
+                        // Record containing TextBox values to output
+                        var record = new Record(accountNumber,
+                            values[(int)TextBoxIndices.First],
+                            values[(int)TextBoxIndices.Last],
+                            decimal.Parse(values[(int)TextBoxIndices.Balance]));
+
+                        // Write Record to file, fields separted by commas
+                        fileWriter.WriteLine(
+                            $"{record.Account},{record.FirstName}," +
+                            $"{record.LastName},{record.Balance}");
+                    }
+                    else
+                    {
+                        // Notify user if invalid account number
+                        MessageBox.Show("Invalid Account Number", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error writing to file", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Invalid Format", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            ClearTextBoxes(); // Clear TextBox values
+        }
+
+        // Handler for exitButton Click
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fileWriter?.Close();  // Close StreamWriter and underlying file
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot close file", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Application.Exit();
         }
     }
 }
